676f1430f60585d5bf45ed7563e36b71d050aab5
alballena <0xalballena@gmail.com>
2017-11-10 10:52:53 -0800

    Initial commit.

---
+++ Assets/DarkRace/Scripts/NormalVelocity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalVelocity : MonoBehaviour {
+    [SerializeField]
+    private float velocity = 0;
+
+    void Update() {
+        transform.Translate(velocity * Time.deltaTime, 0, 0);
+    }
+}
